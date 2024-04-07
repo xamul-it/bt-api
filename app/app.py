9b8229ae9ca9c058f321d0d954628e8d174ebf05
xamul <luigi.zanderighi@gmail.com>
2024-04-07 19:03:48 +0200

    Flask app

---
+++ app/app.py
@@ -0,0 +1,22 @@
+import os
+import json
+from flask import Flask
+from flask_cors import CORS
+from datetime import datetime
+from tickers import tk_bp
+from benchmark import bm_bp
+
+app = Flask(__name__)
+CORS(app)  # Abilita CORS per tutto il tuo applicativo Flask
+app.register_blueprint(bm_bp, url_prefix='/bm')
+app.register_blueprint(tk_bp, url_prefix='/tk')
+
+
+
+@app.route('/')
+def home():
+    return "Ciao, mondo!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/tickers.py
@@ -0,0 +1,124 @@
+import os
+import json
+from flask import jsonify, request, send_from_directory, Blueprint
+from flask_cors import CORS
+from datetime import datetime
+
+tk_bp = Blueprint('tickers', __name__)
+CORS(tk_bp)  # Abilita CORS per tutto il tuo applicativo Flask
+
+def create_index_file(path):
+    # Genera una lista di dizionari per ogni file nella cartella tickers
+    tickers = []
+    for filename in os.listdir(path):
+        if filename.endswith(".json") and filename != "index.json":
+            file_path = os.path.join(path, filename)
+            try:
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+                    num_items = len(data) if isinstance(data, list) else 0
+            except json.JSONDecodeError:
+                num_items = 0  # Se il file non è un JSON valido o non è un array
+
+
+            stats = os.stat(file_path)
+            tickers.append({
+                'name': filename[:-5],
+                'created': datetime.fromtimestamp(stats.st_ctime).strftime('%Y-%m-%d %H:%M:%S'),
+                'updated': datetime.fromtimestamp(stats.st_mtime).strftime('%Y-%m-%d %H:%M:%S'),
+                'num': num_items,
+                'avatar': '',
+                'des': filename
+            })
+
+    # Scrive i dati nel file index.json
+    with open(os.path.join(path, 'index.json'), 'w') as f:
+        json.dump(tickers, f, indent=4)
+
+# Percorso relativo al file JSON
+tickers_path = os.path.join(tk_bp.root_path, '..', 'tickers')
+
+@tk_bp.route('/get-tickerls')
+def get_tickers():
+    # Percorso relativo al file JSON
+    json_file_path = os.path.join(tickers_path, 'index.json')
+
+    # Crea il file index.json se non esiste
+    if not os.path.isfile(json_file_path):
+        create_index_file(tickers_path)
+
+    try:
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+            return jsonify(data)
+    except FileNotFoundError:
+        return jsonify({"error": "File non trovato"}), 404
+    except json.JSONDecodeError:
+        return jsonify({"error": "Formato file non valido"}), 500
+
+@tk_bp.route('/update-tickerls/<list_name>', methods=['POST'])
+def update_ticker(list_name):
+    data_to_update = request.json
+    file_path = os.path.join(tickers_path, 'index.json')
+
+    if not os.path.isfile(file_path):
+        return jsonify({"error": "File non trovato"}), 404
+
+    try:
+        found = False
+        with open(file_path, 'r+') as file:
+            data = json.load(file)
+            # Trova l'elemento da aggiornare nell'array
+            for item in data:
+                if item['name'].lower() == list_name.lower():
+                    item.update(data_to_update)
+                    item['updated'] = datetime.now().strftime('%d/%m/%Y')  # Aggiorna la data di modifica
+                    found = True
+                    break
+            file.seek(0)  # Riporta il puntatore all'inizio del file
+            json.dump(data, file, indent=4)
+            file.truncate()  # Rimuovi il contenuto in eccesso
+        if found:
+            return jsonify({"success": "Dati aggiornati correttamente"})
+        else:
+            return jsonify({"fail": f"Record {list_name} non trovato"}), 404
+    except json.JSONDecodeError:
+        return jsonify({"error": "Errore nella lettura del file JSON"}), 500
+
+@tk_bp.route('/tickerls/<ticker_name>')
+def get_ticker(ticker_name):
+    file_path = os.path.join(tickers_path, f'{ticker_name}.json')
+
+    # Verifica che il file esista
+    if not os.path.isfile(file_path):
+        return jsonify({"fail": f"Record {ticker_name} non trovato"}), 404
+        #abort(404)  # Restituisce un errore 404 se il file non esiste
+
+    # Restituisce il contenuto del file
+    return send_from_directory(tickers_path, f'{ticker_name}.json')
+
+
+
+@tk_bp.route('/update-tickers/<list_name>', methods=['POST'])
+def update_tickers(list_name):
+    # Ottieni l'array di valori dal corpo della richiesta
+    values = request.json
+
+    # Costruisci il percorso del file
+    file_path = os.path.join(tickers_path, f'{list_name}.json')
+
+    # Verifica che i valori siano un array
+    if not isinstance(values, list):
+        return jsonify({"error": "I dati forniti non sono un array"}), 400
+    
+    # Assicurati che la directory esista
+    if not os.path.exists(tickers_path):
+        os.makedirs(tickers_path)
+
+    # Scrivi l'array nel file JSON
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(values, file, indent=4)
+        return jsonify({"message": "Dati salvati correttamente"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
