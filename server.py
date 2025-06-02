c697a7b338e322c955027884be3ae10d6453d385
Xamul <luigi.zanderighi@gmail.com>
2025-06-02 12:33:46 +0200

    test

--- server.py
+++ server.py
@@ -100,6 +100,7 @@ GITHUB_SECRET = b'ssQroXoUKHRspjsONB9bQiyHmjK6nrh1'  # Sostituisci con il secret
 REPO_PATH = "/home/htpc/backtrader"
 SERVICE_NAME = "zmq-proxy"  # es. "myapp.service"
 
+
 def verify_signature(payload, signature):
     if signature is None:
         return False
